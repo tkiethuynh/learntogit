3d07ccdde00b2c247df7abcd5109a35b736da4d3
tkiethuynh <huynh_tuankiet33@yahoo.com>
2021-10-22 14:35:47 +0900

    fdfd

--- Basics/design_kalman_2.py
+++ Basics/design_kalman_2.py
@@ -1,3 +1,5 @@
+import numpy.random as random
+from numpy import array, asarray
 from kf_book.book_plots import plot_residual_limits, set_labels
 from kf_book.book_plots import plot_kf_output
 from filterpy.common import Saver
@@ -130,14 +132,38 @@ def simulate_acc_system(R, Q, count):
     return np.asarray(xs), zs
 
 
-np.random.seed(124)
-xs, zs = simulate_acc_system(R=R, Q=Q, count=80)
+def fusion_test(wheel_sigma, ps_sigma, do_plot=True):
+    dt = 0.1
+    kf = KalmanFilter(dim_x=2, dim_z=2)
 
-kf3 = FirstOrderKF(R, Q * 10, dt=1)
-data3= filter_data(kf3, zs)
+    kf.F = array([[1., dt], [0., 1.]])
+    kf.H = array([[1., 0.], [1., 0.]])
+    kf.x = array([[0.], [1.]])
+    kf.Q *= array([[(dt**3)/3, (dt**2)/2],
+                   [(dt**2)/2,  dt]]) * 0.02
+    kf.P *= 100
+    kf.R[0, 0] = wheel_sigma**2
+    kf.R[1, 1] = ps_sigma**2
+    s = Saver(kf)
 
-plot_kf_output(xs, data3.x, data3.z, aspect_equal=False)
-plot_residuals(xs[:, 0], data3, 0, 
-               'First Order Position Residuals',
-               'meters') 
+    random.seed(1123)
+    for i in range(1, 100):
+        m0 = i + randn()*wheel_sigma
+        m1 = i + randn()*ps_sigma
+        kf.predict()
+        kf.update(array([[m0], [m1]]))
+        s.save()
+    s.to_array()
+    print(f'fusion std: {np.std(s.y[:, 0]):.3f}')
+    if do_plot:
+        ts = np.arange(0.1, 10, .1)
+        plot_measurements(ts, s.z[:, 0], label='Wheel')
+        plt.plot(ts, s.z[:, 1], ls='--', label='Pos Sensor')
+        plot_filter(ts, s.x[:, 0], label='Kalman filter')
+        plt.legend(loc=4)
+        plt.ylim(0, 100)
+        set_labels(x='time (sec)', y='meters')
+
+
+fusion_test(1.5, 3.0)
 plt.show()
